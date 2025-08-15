3874dc11730294404f78e966b0270e485ad8a007
aadriellyy <limaadrielly214@gmail.com>
2025-08-14 22:52:30 -0300

    :sparkles: feat: alt precos

--- landing-page-site/src/js/script.js
+++ landing-page-site/src/js/script.js
@@ -44,4 +44,18 @@ $(document).ready(function(){
         window.open(whatsappUrl, '_blank');
     });
 
+    const sections = $('section');
+    const navLinks = $('.nav-item');
+
+    $(window).on('scroll', function() {
+        const header = $('header');
+        const scrollPosition= $(window).scrollTop() - header.outerHeight();
+        
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else{
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+        }
+    });
+
 });
